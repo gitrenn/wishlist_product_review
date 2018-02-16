b6bcbcc9fc4769c5cf96db46d6ee8f90cbfd9840
Ren <rjingmembers@gmail.com>
2018-02-16 11:20:59 -0800

    added a search button to trigger search on click

--- src/Components/search_bar.js
+++ src/Components/search_bar.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { Input } from 'semantic-ui-react';
+import { Form, Input, Icon, Button } from 'semantic-ui-react';
 
 
 class SearchBar extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { term: ''}
+        this.state = { term: ''};
+        this.onInputChange = this.onInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     onInputChange = (event) => {
@@ -14,18 +16,36 @@ class SearchBar extends Component {
         this.setState({
             term: input
         });
-        this.props.onSearchTermChange(input);
+        //this.props.onSearchTermChange(input);
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.onSearchTermChange(this.state.term);
+        this.setState({ term: ''});
     }
 
     render (){
         return (
             <div>
-                <Input 
-                    onChange={this.onInputChange}
-                    value={this.state.term}
-                    icon='cart' 
-                    placeholder='Search...' />
-                Value of the input: { this.state.term }
+                <Form>
+                    <Input 
+                        onChange={this.onInputChange}
+                        value={this.state.term}
+                        icon='search' 
+                        placeholder='Search...' />                     
+                    <Button.Group floated='right'>
+                        <Button
+                            onClick={this.handleSubmit} 
+                            type='submit' 
+                            animated>
+                            <Button.Content visible>Search</Button.Content>
+                            <Button.Content hidden>
+                                <Icon name='youtube' /> YouTube
+                            </Button.Content>
+                        </Button>
+                    </Button.Group>
+                </Form>
             </div>
         )
 
